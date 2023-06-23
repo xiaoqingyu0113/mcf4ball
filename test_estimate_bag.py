17329d5b2814365898b72cd9323f24f327070e6a
Qingyu Xiao <xiaoqingyu0113@gmail.com>
2023-06-23 12:54:53 -0400

    init

--- test_estimate_bag.py
+++ test_estimate_bag.py
@@ -4,6 +4,7 @@ import numpy as np
 import os
 import matplotlib.pyplot as plt
 import time
+from collections import deque
 
 from test_makedata import make_data
 
@@ -33,6 +34,22 @@ def init_camera_params():
 
     return camera_param_list
 
+def check_optimizable(obs_buffer,graph_minimum_size = 150):
+    prev_camera_id = int(obs_buffer[0][1])
+    sum_change = 0
+    N = len(obs_buffer)
+    for idx in range(N):
+        curr_camera_id = int(obs_buffer[idx][1])
+        if prev_camera_id != curr_camera_id:
+            sum_change += 1
+    print(f'check optimizable ({sum_change/graph_minimum_size:.2f})')
+    return sum_change/graph_minimum_size > 0.6
+
+def add_buffer(obs_buffer:deque,data,graph_minimum_size = 150):
+    if len(obs_buffer) >= graph_minimum_size:
+        obs_buffer.popleft()
+    obs_buffer.append(data)
+        
 
 def main():
     data_array = load_data()
@@ -44,24 +61,30 @@ def main():
     saved_w = []
 
     graph_minimum_size = 150
-    gtsam_solver = IsamSolver(camera_param_list)
+    gtsam_solver = IsamSolver(camera_param_list,verbose=False)
     total_iter = len(data_array)
-    prev_cam_id = 0
     total_time = -time.time()
+
+    obs_buffer = deque()
+    optimizable = False
     for iter,data in enumerate(data_array):
         print(f"\niter = {iter}")
-        if iter < 200:
-            continue
-        # print(f"{float(data[0])} > {gtsam_solver.t_max} = {float(data[0]) > gtsam_solver.t_max}")
+        add_buffer(obs_buffer,data)
+        if not optimizable: 
+            optimizable = check_optimizable(obs_buffer)
+        print(optimizable)
         if  (int(data[1])==3) or (int(data[1])==4) or (int(data[1])==5) :
             continue
         if iter > 2000:
             break
-        if float(data[0]) > gtsam_solver.t_max:
-            if gtsam_solver.curr_node_idx < graph_minimum_size:
+        if (float(data[0]) > gtsam_solver.t_max) and optimizable:
+            # if iter < 0:
+            #     continue
+            print(gtsam_solver.curr_node_idx)
+            if (gtsam_solver.curr_node_idx < graph_minimum_size):
                 gtsam_solver.update(data,optim=False)
             else:
-                gtsam_solver.update(data,optim=True)
+                gtsam_solver.update(data,optim=optimizable)
                 rst = gtsam_solver.get_result()
                 p_rst,v_rst,w_rst = rst
                 if np.linalg.norm(w_rst) > 800:
@@ -69,7 +92,6 @@ def main():
                 saved_p.append(p_rst)
                 saved_w.append(w_rst)
                 saved_v.append(v_rst)
-        prev_cam_id = int(data[1])
     total_time += time.time()
     print('average inference time (hz) = ', (total_iter-graph_minimum_size)/total_time)
 
@@ -80,34 +102,34 @@ def main():
 
     print(saved_p.shape)
     # -------------------------------------- plain plot ------------------------------
-    comet(saved_p[::3],saved_v[::3],saved_w[::3],predict_trajectory)
-    # fig = plt.figure()
-    # ax = fig.add_subplot(projection='3d')
+    # comet(saved_p[::3],saved_v[::3],saved_w[::3],predict_trajectory)
+    fig = plt.figure()
+    ax = fig.add_subplot(projection='3d')
 
 
-    # ax.plot(saved_p[:,0], saved_p[:,1],saved_p[:,2], 'b.', markerfacecolor='black', markersize=3)
-    # ax.set_title('uses cameras 1-3')
-    # set_axes_equal(ax)
-    # ax.set_xlabel('x');ax.set_ylabel('y');ax.set_zlabel('z')
-    # plt.show()
+    ax.plot(saved_p[:,0], saved_p[:,1],saved_p[:,2], 'b.', markerfacecolor='black', markersize=3)
+    ax.set_title('uses cameras 1-3')
+    set_axes_equal(ax)
+    ax.set_xlabel('x');ax.set_ylabel('y');ax.set_zlabel('z')
+    plt.show()
 
 
-    # fig, axs = plt.subplots(1, 2)  # This will create a 2x2 grid of Axes objects
-    # labels = ['x','y','z']
-    # gt_color = ['r','g','b']
+    fig, axs = plt.subplots(1, 2)  # This will create a 2x2 grid of Axes objects
+    labels = ['x','y','z']
+    gt_color = ['r','g','b']
 
-    # for i in range(3):
-    #     axs[0].plot(saved_v[:,i],'o',label=labels[i])
+    for i in range(3):
+        axs[0].plot(saved_v[:,i],'o',label=labels[i])
 
-    #     axs[1].plot(saved_w[:,i]/(2*np.pi),'o',label=labels[i]) # convert to herz
+        axs[1].plot(saved_w[:,i]/(2*np.pi),'o',label=labels[i]) # convert to herz
 
-    # axs[0].set_ylabel('linear velocity (m/s)')    
-    # axs[1].set_ylabel('angular velocity (Hz)')    
+    axs[0].set_ylabel('linear velocity (m/s)')    
+    axs[1].set_ylabel('angular velocity (Hz)')    
 
-    # axs[0].legend() 
-    # axs[1].legend() 
+    axs[0].legend() 
+    axs[1].legend() 
 
-    # plt.show()
+    plt.show()
 
 if __name__ == '__main__':
     main()
